4dc03079dac50dd87c860405f267cc15ba67120f
Jurre Wolff <jurre.wolff@hva.nl>
2020-04-09 15:22:05 +0200

    Implemented game logic.

--- RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
+++ RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.rockpaperscissors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        btnRock.setOnClickListener{ onUserMove(0) }
+        btnPaper.setOnClickListener{ onUserMove(1) }
+        btnScissors.setOnClickListener{ onUserMove(2) }
+    }
+
+    private fun onUserMove(userMove: Int) {
+        val winner = winner(userMove, computerMove())
+        Toast.makeText(this, "The winner is: $winner", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun computerMove(): Int {
+        return Random.nextInt(0, 3)
+    }
+
+    private fun winner(userMove: Int, computerMove: Int): String{
+        if (computerMove == userMove) return "draw"
+
+        return when(userMove) {
+            0 -> if (computerMove == 1) {"computer"} else "user"
+            1 -> if (computerMove == 2) {"computer"} else "user"
+            2 -> if (computerMove == 0) {"computer"} else "user"
+            else -> "IMPOSSIBLE!!!"
+        }
     }
 }
